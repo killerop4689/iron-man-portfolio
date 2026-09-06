edb4b4c0f4e880ba5bd911194e25274f081fa66b
Rahul R Sirimane <rahulsirimane@gmail.com>
2026-09-06 20:52:20 +0530

    final working app - hopefully

--- js/topbar.js
+++ js/topbar.js
@@ -3,10 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
     // 1. RUN 24-HOUR MILITARY CLOCK ENGINE
     const clockEl = document.getElementById('hud-clock');
     function updateClock() {
-        const now = new Date();
-        const hours = String(now.getHours()).padStart(2, '0');
-        const minutes = String(now.getMinutes()).padStart(2, '0');
-        clockEl.innerText = `${hours}:${minutes}`;
+     const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    clockEl.innerText = `${hours}:${minutes}:${seconds}`;
     }
     updateClock();
     setInterval(updateClock, 1000); // Ticks every second
